ad7c5f9cc7b6942137ceab02a89bdf0d5e7c3f2c
Nicola De Angeli <112023843+niqodea@users.noreply.github.com>
2023-12-29 16:08:43 +0100

    feat: add delta applications

---
+++ apps/delta/src/org/app/delta/main.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+from org.lib.bar import get_primes
+from org.lib.foo import load_image
+
+from .utils import ASSETS_PATH
+
+
+def main() -> None:
+    for image_path in ASSETS_PATH.iterdir():
+        image = load_image(image_path)
+
+        np_image = np.array(image)
+
+        mean_intensity = int(np_image.mean())
+
+        print(f"Mean intensity of {image_path.name} is {mean_intensity}")
+
+        for prime in get_primes():
+            if mean_intensity % prime == 0:
+                print(f"Mean intensity {mean_intensity} is divisible by {prime}")
+            else:
+                print(f"Mean intensity {mean_intensity} is not divisible by {prime}")
+
+
+if __name__ == "__main__":
+    main()
